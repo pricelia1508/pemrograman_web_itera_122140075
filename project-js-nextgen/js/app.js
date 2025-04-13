a44db20b7d52eb850a43bae3c832958637cafdc4
pricelia1508 <pricelia.122140075@student.itera.ac.id>
2025-04-13 21:15:59 +0700

    Delete project-js-nextgen directory

--- project-js-nextgen/js/app.js
+++
@@ -1,8 +0,0 @@
-import { getData, saveData } from './modules/data.js';
-import { renderDashboard } from './modules/utils.js';
-
-export const initApp = () => {
-  const app = document.getElementById('app');
-  app.innerHTML = `<h1>Personal Dashboard</h1>`;
-  renderDashboard(app);
-};
